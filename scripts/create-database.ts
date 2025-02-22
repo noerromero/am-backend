660b819b5dce1964e38c297ebf8a98ddbd9a01f1
noerromero <noerromero@gmail.com>
2025-02-21 21:47:16 -0500

    add db-reset script

--- scripts/create-database.ts
+++ scripts/create-database.ts
@@ -10,6 +10,9 @@ const host = process.env.DB_HOST as string;
 const port = process.env.DB_PORT as string;
 const dialect = 'mysql';
 
+// Leer el argumento de la línea de comandos para eliminar la base de datos
+const shouldDropDatabase = process.argv.includes('--drop');
+
 const sequelize = new Sequelize(database, username, password, {
   host: host,
   port: parseInt(port), // Specify the port here
@@ -26,6 +29,12 @@ const createDatabase = async () => {
       dialect: dialect,
       logging: false,
     });
+
+    // Drop the database if it exists and the flag is set
+    if (shouldDropDatabase) {
+      await sequelizeWithoutDb.query(`DROP DATABASE IF EXISTS ${database};`);
+      console.log(`Database ${database} dropped.`);
+    }
 
     // Create the database if it doesn't exist
     await sequelizeWithoutDb.query(`CREATE DATABASE IF NOT EXISTS ${database};`);
